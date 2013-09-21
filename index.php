c497c7ba7b5010a28dd30a8e7a857f122ae27e1d
netsurfer912 <netsurfer912@gmail.com>
2013-09-21 02:16:52 +0200

    stuff
    Added resolution-specific CSS, added a pretty headline and removed the need for an extra directory, all the images get uploaded into the directory of the script (for better sharing). Also pushed the size limit to 50MB (a lot).

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
 <?php
 // Configuration
 $title = 'Image Uploader';
-$filedir = 'up';
-$maxsize = 5242880; //max size in bytes
+$maxsize = 52428800; //max size in bytes
 $allowedExts = array('png', 'jpg', 'jpeg', 'gif');
 $allowedMime = array('image/png', 'image/jpeg', 'image/pjpeg', 'image/gif');
-$baseurl = $_SERVER['HTTP_HOST'].dirname($_SERVER['REQUEST_URI']).'/'.$filedir;
+$baseurl = $_SERVER['HTTP_HOST'].dirname($_SERVER['REQUEST_URI'])
 ?>
 <html>
 <head>
+	<!-- This simple image service is brought to you by netsurfer912, based on https://github.com/Spittie/simple-php-image-hosting-->
     <title><?php print $title; ?></title>
-<link rel="stylesheet" href="css.css" type="text/css" />
+<link rel='stylesheet' href='normal.css' />
+<link rel='stylesheet' media='(max-width: 800px)' href='small.css' />
 </head>
 <body>
+	<h1>Upload your image. </h1>
 	<div id="upload">
 		<form enctype="multipart/form-data" action="<?php print $_SERVER['PHP_SELF']; ?>" method="POST">
-		<input type="hidden" name="MAX_FILE_SIZE" value="5242880" />
+		<input type="hidden" name="MAX_FILE_SIZE" value="52428800" />
 		Choose a file to upload: <br />
 		<input size="62"  name="file" type="file" accept="image/*" />
-		<input type="submit" value="Upload File" />
+		<input type="submit" value="Upload Image" />
 		</form>
 		<div id="info">
-		Max file size: 5mb <br/>
-		Supported formats: png, jpg, gif <br/>
-		Please don't upload anything illegal
+		Maximal file size: 50MB <br/>
+		All images allowed <br/>
 		</div>
 	</div>
 	<div id="image">
@@ -38,23 +39,28 @@ $baseurl = $_SERVER['HTTP_HOST'].dirname($_SERVER['REQUEST_URI']).'/'.$filedir;
 		&& ($_FILES['file']['size'] <= $maxsize)) {
 			$md5 = substr(md5_file($_FILES['file']['tmp_name']), 0, 7);
 			$newname = time().$md5.'.'.$ext;
-			move_uploaded_file($_FILES['file']['tmp_name'], $filedir.'/'.$newname);
-			$baseurl = $_SERVER['HTTP_HOST'].dirname($_SERVER['REQUEST_URI']).'/'.$filedir;
+			move_uploaded_file($_FILES['file']['tmp_name'], $newname);
+			$baseurl = $_SERVER['HTTP_HOST'].dirname($_SERVER['REQUEST_URI']);
 			$imgurl = 'http://'.$baseurl.'/'.$newname;
 			print '<br />';
-			print 'Your URL:<br />';
-			print '<input type="text" value="'.$imgurl.'" ><br /><br />';
+			print 'Direct link:<br />';
+			print '<input type="text" value="'.$imgurl.'" readonly="readonly"><br /><br />';
+			print 'BB-Code:<br />';
+			print '<input type="text" value="[img]'.$imgurl.'[/img]" readonly="readonly"><br /><br />';
+			print 'Preview:<br />';
 			print '<a href="'.$imgurl.'"><img src="'.$imgurl.'" /></a><br />';
 		}
 
 		else {
-			print '<br />';
-			print 'Something went wrong <br />';
+			print '<p>';
+			print '<strong lass="error">Something went wrong. If this keeps happening, please contact me. </strong>';
+			print '</p>';
 		}
 		
 	}
 ?>
 	</div>
+	<div id="footnote">This simple image sharing service is brought to you by netsurfer912. </div>
 </body>
 </html>
 
